22bc58df6c17e7152941e73724ebdb219e4316ce
deplax <sturdyegg@gmail.com>
2018-10-02 17:09:22 +0900

    [dump]dump commit

---
+++ src/main/java/javastatic/LongTest.java
@@ -0,0 +1,9 @@
+package javastatic;
+
+public class LongTest {
+    public Boolean longTest() {
+        String s1 = "hello";
+        String s2 = "hello";
+        return s1.equals(s2);
+    }
+}
---
+++ src/main/java/javastatic/Main.java
@@ -0,0 +1,11 @@
+package javastatic;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("test");
+        String a = "asdf1234";
+        String [] str = a.split("1");
+        System.out.println(a);
+        System.out.println(str);
+    }
+}
